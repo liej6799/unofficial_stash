414bf11f608ed3c4a9351e994b93dc61fc079787
liej6799 <52598803+liej6799@users.noreply.github.com>
2020-06-18 20:52:13 +0800

    add linux file support, pip

--- module/common/common_config.py
+++ module/common/common_config.py
@@ -2,14 +2,14 @@ import pathlib
 
 
 def get_json_base_path():
-    return str(pathlib.Path(__file__).parent.absolute()) + "\\urls.json"
+    return str(pathlib.Path(__file__).parent.absolute()) + "/urls.json"
 
 def get_api_status_base_path():
-    return str(pathlib.Path(__file__).parent.absolute()) + "\\api_status.json"
+    return str(pathlib.Path(__file__).parent.absolute()) + "/api_status.json"
 
 def get_language_base_path():
-    return str(pathlib.Path(__file__).parent.absolute()) + "\\languages"
+    return str(pathlib.Path(__file__).parent.absolute()) + "/languages"
 
 def get_secret_base_path():
-    return str(pathlib.Path(__file__).parent.absolute()) + "\\creds.json"
+    return str(pathlib.Path(__file__).parent.absolute()) + "/creds.json"
 
